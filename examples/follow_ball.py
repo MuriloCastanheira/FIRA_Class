d6ae8596b8d4a4b5530407721ec21d13d4f370ee
MuriloCastanheira <murilobicho@gmail.com>
2022-11-21 09:45:03 -0300

    Multicast

--- examples/follow_ball.py
+++ examples/follow_ball.py
@@ -1,9 +1,9 @@
 import sys
-sys.path.insert(0,"src")
+sys.path.insert(0,"/home/murilo/Documentos/vsss_ws/FIRA_Class/src")
 from Communication import Communication
-from go_to import go_to
+from potential_field import potential_field
 
 while True:
     ball = Communication().ball()
-    go = go_to(0, True, ball.x, ball.y, 0)
+    go = potential_field(0, False, ball.x, ball.y, 0)
     go.go_to()
--- src/Communication.py
+++ src/Communication.py
@@ -1,7 +1,7 @@
 from re import A
 import socket
 import sys
-sys.path.insert(0,"src/protos")
+sys.path.insert(0,"/home/murilo/Documentos/vsss_ws/FIRA_Class/src/protos")
 
 from common_pb2 import Frame
 from packet_pb2 import Environment, Packet
